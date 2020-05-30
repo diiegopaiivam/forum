f8cd545b734e24dd25e689901fa0149428047581
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-05-30 17:27:15 -0300

    extraindo templates

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,3 +1,12 @@
-<example-component>
-    carregando
-</example-component>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+
+<div id="app">
+    <example-component>
+        carregando
+    </example-component>
+</div>
+
+<script src="/js/manifest.js"></script>
+<script src="/js/vendor.js"></script>
+<script src="/js/bootstrap.js"></script>
+<script src="/js/threads.js"></script>
